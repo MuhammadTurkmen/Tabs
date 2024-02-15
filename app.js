5bb158eaa673da5e8459e71fa56516cc5eb9d696
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-15 07:34:54 +0430

    init

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const btns = document.querySelectorAll('.tab-btn')
 const about = document.querySelector('.about')
 const articles = document.querySelectorAll('.content')
+
+about.addEventListener('click', function(e) {
+    
+})
